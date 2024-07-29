61caf110ef6e3a0464bb005bdf6546d04c4d2858
XWIN\x <xxlinxx@outlook.com>
2024-07-30 01:23:57 +0800

    [add]apiUri and apiKey in setup tab

--- scripts/script.py
+++ scripts/script.py
@@ -11,12 +11,15 @@ import re
 
 from modules.processing import StableDiffusionProcessingTxt2Img
 
+
 def _get_effective_prompt(prompts: list[str], prompt: str) -> str:
     return prompts[0] if prompts else prompt
+
 
 class JSPromptScript(scripts.Script):
     LLM_YOU = gr.State("a1")
     client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
     llm_history_array = []
     defaultJS = """%% {
     resultX = await fetch('http://localhost:1234/v1/chat/completions', {
@@ -51,24 +54,26 @@ class JSPromptScript(scripts.Script):
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
+
     def call_llm_pythonlib(self, _llm_system_prompt, _llm_ur_prompt, _llm_max_token):
         completion = self.client.chat.completions.create(
-            model="CHE-72/Qwen1.5-4B-Chat-Q2_K-GGUF/qwen1.5-4b-chat-q2_k.gguf",
+            model="",
             messages=[
                 {"role": "system", "content": _llm_system_prompt},
                 {"role": "user", "content": _llm_ur_prompt}
             ],
             max_tokens=_llm_max_token,
             temperature=0.7,
+
         )
         print("\n\n[][Init-UI][completion]: " + str(completion) + "\n\n")
-        self.llm_history_array.append([completion.choices[0].message.content, _llm_ur_prompt, _llm_system_prompt] )
+        self.llm_history_array.append([completion.choices[0].message.content, _llm_ur_prompt, _llm_system_prompt])
         if len(self.llm_history_array) > 3:
             self.llm_history_array.remove(self.llm_history_array[0])
         return completion.choices[0].message.content, self.llm_history_array
 
     def ui(self, is_img2img):
-        print("\n\n[][Init-UI][sd-webui-prompt-auto-llm]: " + str(is_img2img) + "\n\n")
+        # print("\n\n[][Init-UI][sd-webui-prompt-auto-llm]: " + str(is_img2img) + "\n\n")
         examples = [
             ["The Moon's orbit around Earth has"],
             ["The smooth Borealis basin in the Northern Hemisphere covers 40%"],
@@ -77,78 +82,90 @@ class JSPromptScript(scripts.Script):
         # def List_LLM_Models():
         #     completion = self.client.models.models()
         #     return completion
-
         def filter_records(records, gender):
             return records[records["gender"] == gender]
 
         with gr.Blocks():
             # gr.Markdown("Blocks")
-            with gr.Accordion(open=True, label="Auto LLM Prompt v20240723"):
-                with gr.Accordion(open=True, label="[Prompt]/[LLM-PythonLib]"):
-                    gr.Markdown("Generate inside Main Prompt")
-                    llm_is_enabled = gr.Checkbox(label="Enable LLM to prompt", value=True)
 
-                    with gr.Row():
-                        llm_system_prompt = gr.Textbox(label="0. [System-Prompt]", lines=10,
-                                                       value="You are an AI prompt word "
-                                                             "engineer. Use the provided "
-                                                             "keywords to create a beautiful "
-                                                             "composition. Only the prompt "
-                                                             "words are needed, "
-                                                             "not your feelings. Customize the "
-                                                             "style, scene, decoration, etc., "
-                                                             "and be as detailed as possible "
-                                                             "without endings.",
-                                                       placeholder="You are an AI prompt word "
-                                                                   "engineer. Use the provided "
-                                                                   "keywords to create a beautiful "
-                                                                   "composition. Only the prompt "
-                                                                   "words are needed, "
-                                                                   "not your feelings. Customize the "
-                                                                   "style, scene, decoration, etc., "
-                                                                   "and be as detailed as possible "
-                                                                   "without endings."
-                                                       )
-                        with gr.Column():
-                            llm_ur_prompt = gr.Textbox(label="1. [YOU-Prompt]", lines=3, value="A superstar on stage.",
-                                                       placeholder="A superstar on stage.")
-                            llm_llm_answer = gr.Textbox(inputs=self.process, show_copy_button=True, interactive=True, label="2. [LLM-Answer]", lines=3, placeholder="LLM says.")
+                with gr.Accordion(open=True, label="Auto LLM Prompt v20240723"):
+                    with gr.Tab("Prompt/LLM-PythonLib"):
+                        # with gr.Accordion(open=True, label="[Prompt]/[LLM-PythonLib]"):
+                        gr.Markdown("* Generate forever mode \n"
+                                    "* Story board mode")
+                        llm_is_enabled = gr.Checkbox(label="Enable LLM-Answer to SD-prompt", value=True)
 
-                    llm_history = gr.Dataframe(
-                        interactive=True,
-                        label="History",
-                        headers=["llm_answer", "system_prompt", "ur_prompt"],
-                        datatype=["str", "str", "str"],
-                        row_count=3,
-                        col_count=(3, "fixed"),
-                    )
+                        with gr.Row():
+                            with gr.Column():
 
-                    llm_max_token = gr.Slider(5, 500, value=50, step=5, label="Max length w/Generate token ")
+                                llm_system_prompt = gr.Textbox(label="1. [LLM-System-Prompt]", lines=10,
+                                                               value="You are an AI prompt word "
+                                                                     "engineer. Use the provided "
+                                                                     "keywords to create a beautiful "
+                                                                     "composition. Only the prompt "
+                                                                     "words are needed, "
+                                                                     "not your feelings. Customize the "
+                                                                     "style, scene, decoration, etc., "
+                                                                     "and be as detailed as possible "
+                                                                     "without endings.",
+                                                               placeholder="You are an AI prompt word "
+                                                                           "engineer. Use the provided "
+                                                                           "keywords to create a beautiful "
+                                                                           "composition. Only the prompt "
+                                                                           "words are needed, "
+                                                                           "not your feelings. Customize the "
+                                                                           "style, scene, decoration, etc., "
+                                                                           "and be as detailed as possible "
+                                                                           "without endings."
+                                                               )
+                            with gr.Column():
+                                llm_ur_prompt = gr.Textbox(label="2. [Your-Prompt]", lines=2, value="A superstar on stage.",
+                                                           placeholder="A superstar on stage.")
+                                llm_llm_answer = gr.Textbox(inputs=self.process, show_copy_button=True, interactive=True,
+                                                            label="3. [LLM-Answer]", lines=6, placeholder="LLM says.")
 
-                    llm_button = gr.Button("Call LLM above")
-                    llm_button.click(self.call_llm_pythonlib, inputs=[llm_system_prompt, llm_ur_prompt, llm_max_token],
-                                     outputs=[llm_llm_answer, llm_history])
-                with gr.Accordion(open=False, label="[Prompt]/[LLM-JS]"):
-                    gr.Markdown("Generate by JS / each generate")
-                    js_is_enabled = gr.Checkbox(label=" Enable Dynamic Javascript", value=False)
-                    js_howMany = gr.Slider(1, 20, value=2, step=1,
-                                           label=" When Generate forever => ImagesCount/eachResult",
-                                           info="repet use. (When call this JS get result; keep use the result on how many times)")
-                    js_template = gr.Dropdown(
-                        ['online'].append("online2"), value=['online'], multiselect=False,
-                        label="List JS template(online)",
-                        info="online URI: https://decade.tw/js_template"
-                    )
-                    with gr.Row():
-                        js_prompt_js = gr.Textbox(label=" f[Prompt-JS]  %%{...}%%", lines=3,
-                                                  placeholder=self.defaultJS)
-                        js_result = gr.Textbox(label=" [Prompt-JS-Result]", lines=3, value="")
-        return [js_is_enabled, llm_is_enabled,
+                        llm_history = gr.Dataframe(
+                            interactive=True,
+                            label="History/StoryBoard",
+                            headers=["llm_answer", "system_prompt", "ur_prompt"],
+                            datatype=["str", "str", "str"],
+                            row_count=3,
+                            col_count=(3, "fixed"),
+                        )
+
+                        llm_max_token = gr.Slider(5, 500, value=50, step=5, label="Max LLM-answer length")
+                        llm_button = gr.Button("Call LLM above")
+                        llm_button.click(self.call_llm_pythonlib, inputs=[llm_system_prompt, llm_ur_prompt, llm_max_token],
+                                         outputs=[llm_llm_answer, llm_history])
+                    with gr.Tab("Prompt/LLM-JS"):
+                        # with gr.Accordion(open=True, label="[Prompt]/[LLM-JS]"):
+                        gr.Markdown("* Generate by JS / each generate \n"
+                                    "* inactive between SD-Prompt and LLM-Prompt")
+
+                        js_is_enabled = gr.Checkbox(label=" Enable Dynamic Javascript", value=False)
+                        js_howMany = gr.Slider(1, 20, value=2, step=1,
+                                               label=" When Generate forever => ImagesCount/eachResult",
+                                               info="repet use. (When call this JS get result; keep use the result on how many times)")
+                        js_template = gr.Dropdown(
+                            ['online'].append("online2"), value=['online'], multiselect=False,
+                            label="List JS template(online)",
+                            info="online URI: https://decade.tw/js_template"
+                        )
+                        with gr.Row():
+                            js_prompt_js = gr.Textbox(label=" f[Prompt-JS]  %%{...}%%", lines=33,
+                                                      placeholder=self.defaultJS)
+                            js_result = gr.Textbox(label=" [Prompt-JS-Result]", lines=33, value="")
+                    with gr.Tab("Setup"):
+                        llm_apiurl = gr.Textbox(label="0. [LLM-URL]", lines=1,
+                                                placeholder="http://localhost:1234/v1")
+                        llm_apikey = gr.Textbox(label="0. [LLM-API-Key]", lines=1,
+                                                placeholder="lm-studio")
+        return [js_is_enabled, llm_is_enabled, llm_apiurl, llm_apikey,
                 js_prompt_js, js_result,
                 llm_system_prompt, llm_ur_prompt, llm_max_token,
                 llm_llm_answer, llm_history]
 
-    def process(self, p: StableDiffusionProcessingTxt2Img, js_is_enabled, llm_is_enabled,
+    def process(self, p: StableDiffusionProcessingTxt2Img, js_is_enabled, llm_is_enabled, llm_apiurl, llm_apikey,
                 js_prompt_js, js_result,
                 llm_system_prompt, llm_ur_prompt, llm_max_token,
                 llm_llm_answer, llm_history):
@@ -156,10 +173,10 @@ class JSPromptScript(scripts.Script):
         if llm_is_enabled:
             r = self.call_llm_pythonlib(llm_system_prompt, llm_ur_prompt, llm_max_token)
             g_result = str(r[0])
-            print("[][][]llm_llm_answer ",llm_llm_answer)
+            print("[][][]llm_llm_answer ", llm_llm_answer)
 
             for i in range(len(p.all_prompts)):
-                p.all_prompts[i] = p.all_prompts[i]+("," if (p.all_prompts[0].__len__() > 0) else "") + g_result
+                p.all_prompts[i] = p.all_prompts[i] + ("," if (p.all_prompts[0].__len__() > 0) else "") + g_result
 
         if js_is_enabled:
             print("[][][]js_is_enabled-true")
@@ -192,6 +209,7 @@ class JSPromptScript(scripts.Script):
             p.prompt_for_display = original_prompt
 
         return p.all_prompts[0]
+
 
 class JavascriptRunner:
     use_same_result_cout = 0
